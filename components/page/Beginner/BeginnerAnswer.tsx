467d7974fe53551a69ff6b7883353a291b774271
taic-dev <taic-dev@taic-devnoMacBook-puro.local>
2023-03-13 19:32:18 +0900

    #5 アンサーページの大枠作成

--- components/page/Beginner/BeginnerAnswer.tsx
+++ components/page/Beginner/BeginnerAnswer.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import styles from "./Beginner.module.scss";
 
 interface BeginnerProps {
   answerArray: string[];
   questionArray: [];
+}
+
+interface Desc {
+  length: number | undefined;
+  genera: string | undefined;
+  flavor: string | undefined;
+  img: string | undefined;
 }
 
 const BeginnerAnswer = ({ answerArray, questionArray }: BeginnerProps) => {
@@ -10,9 +18,17 @@ const BeginnerAnswer = ({ answerArray, questionArray }: BeginnerProps) => {
   console.log(answerArray);
 
   return (
-    <div>
-      Ansewrページ
-
+    <div className={styles.beginner__wrapper}>
+      <h1>▶︎ けっかはっぴょう</h1>
+      <ul className={styles.beginner__list}>
+        {questionArray[0].desc.map((v: Desc,i: React.Key | number)=>(
+          <li className={styles.beginner__item} key={i}>
+            {/* {console.log(v.img)} */}
+            <img src={v.img} alt="回答画像" />
+            <p></p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
