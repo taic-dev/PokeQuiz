4da4ce922bbf4d1061a2a0ac4213051fbecfed31
taic-dev <taic-dev@taic-devnoMacBook-puro.local>
2023-03-30 11:41:09 +0900

    #10 トップページのデザインが完成(配列で画像を管理)

--- components/page/Home/Home.tsx
+++ components/page/Home/Home.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Typography, Box } from "@mui/material";
 import styles from "./Home.module.scss";
+import HomeSlider from "./HomeSlider";
 import Button from "@/components/ui/Button/Button";
 
 const Home = () => {
   return (
     <>
-    <Box className={styles.home}>
-      <Typography className={styles.home__title}>PokeQuiz</Typography>
-      <Box className={styles.home__box}>
-        <Button link="/description">初級者向けクイズ</Button>
-        <Button link="/description">中級者向けクイズ</Button>
-        <Button link="/description">上級者向けクイズ</Button>
-      </Box>
-    </Box>
+      <HomeSlider>
+        <Box className={styles.home}>
+          <Typography className={styles.home__title}>PokeQuiz</Typography>
+          <Box className={styles.home__box}>
+            <Button link="/description">初級者向けクイズ</Button>
+            <Button link="/description">中級者向けクイズ</Button>
+            <Button link="/description">上級者向けクイズ</Button>
+          </Box>
+        </Box>
+      </HomeSlider>
     </>
   );
 };
---
+++ components/page/Home/HomeSlider.tsx
@@ -0,0 +1,122 @@
+import React, { FC, ReactNode } from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
+import styles from "./Home.module.scss";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Images = [
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/123.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/123.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/156.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/187.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/198.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/145.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/441.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/31.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/51.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/61.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/71.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/199.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/190.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+];
+
+const HomeSlider: FC<Props> = ({ children }) => {
+  SwiperCore.use([Autoplay]);
+
+  return (
+    <>
+      <div className={styles.home__swiper}>
+        <Swiper
+          className={styles["home__swiper-container"]}
+          slidesPerView={5}
+          loop={true}
+          direction={"vertical"}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            reverseDirection: true,
+          }}
+        >
+          {Images.map((image: string) => {
+            return (
+              <SwiperSlide>
+                <img src={image} alt="ポケモン画像" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <Swiper
+          className={styles["home__swiper-container"]}
+          slidesPerView={5}
+          loop={true}
+          direction={"vertical"}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {Images.map((image: string) => {
+            return (
+              <SwiperSlide>
+                <img src={image} alt="ポケモン画像" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <Swiper
+          className={styles["home__swiper-container"]}
+          slidesPerView={5}
+          loop={true}
+          direction={"vertical"}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            reverseDirection: true,
+          }}
+        >
+          {Images.map((image: string) => {
+            return (
+              <SwiperSlide>
+                <img src={image} alt="ポケモン画像" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <Swiper
+          className={styles["home__swiper-container"]}
+          slidesPerView={5}
+          loop={true}
+          direction={"vertical"}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {Images.map((image: string) => {
+            return (
+              <SwiperSlide>
+                <img src={image} alt="ポケモン画像" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default HomeSlider;
