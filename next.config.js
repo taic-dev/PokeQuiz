1752e5933cf7a3759e5e3e7e574e904e33f9a991
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-05-28 10:42:36 +0900

    #37 next pwa導入完了

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NEXT_PUBLIC_NODE_ENV === "development",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: [''], //ここにドメインを指定
   },
-}
-
-module.exports = nextConfig
+});
