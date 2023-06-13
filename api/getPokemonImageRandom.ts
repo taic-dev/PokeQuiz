8f8e9738e7ecb1750bbfcbe26d0d01e794fc625d
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-06-13 23:18:28 +0900

    #25 ポケモンの画像被らないようにする

--- api/getPokemonImageRandom.ts
+++ api/getPokemonImageRandom.ts
@@ -2,8 +2,18 @@ import randomNum from "@/util/randomNum";
 
 const getPokemonRandom = (min: number, max: number, count: number): string[]  => {
   const pokemonArray: string[] = [];
+  let num: number;
+  let checkRandomArray: number[] = [];
+
   for(let i=0; i<=count; i++) {
-    let num = randomNum(min, max);
+    // 図鑑Noにかぶりがないか確認
+    while(true) {
+      num = randomNum(min, max);
+      if(!checkRandomArray.includes(num)){
+        checkRandomArray.push(num);
+        break;
+      }
+    }
     let url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${num}.gif`;
     pokemonArray.push(url);
   }
